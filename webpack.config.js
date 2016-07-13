71c15da6d4bce7b7e3a74a82a09077e252335d10
Parit Bansal <parit.bansal@isb-sib.ch>
2016-07-13 11:28:01 +0200

    no index.html generated

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: __dirname + '/app/index.html',
+  filename: 'index.html',
+  inject: 'body'
+});
+
+module.exports = {
+    context: __dirname + "/app",
+    entry: "./main.js",
+
+    output: {
+        filename: "bundle.js",
+        path: __dirname + "/dist"
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            }
+        ],
+        plugins: [HTMLWebpackPluginConfig]
+    }
+};
